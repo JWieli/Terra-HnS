26d2b9acb1a2b13adb0d8b4234919fec7ac2fbd7
Julia Wielińska <j.wielinska18@gmail.com>
2024-05-17 01:14:45 +0200

    Update main.tf
    Vnets

--- main.tf
+++ main.tf
@@ -18,3 +18,42 @@ resource "azurerm_resource_group" "rg" {
     environment = "development"
   }
 }
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnetA"
+  location            = "West Europe"
+  resource_group_name = "JulkaTest1"
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+
+  subnet {
+    name           = "subnetA"
+    address_prefix = "10.0.1.0/24"
+  }
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnetB"
+  location            = "West Europe"
+  resource_group_name = "JulkaTest1"
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+
+  subnet {
+    name           = "subnetB"
+    address_prefix = "10.0.1.0/24"
+  }
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnetC"
+  location            = "West Europe"
+  resource_group_name = "JulkaTest1"
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+
+  subnet {
+    name           = "subnetC"
+    address_prefix = "10.0.1.0/24"
+  }
+}
