0e0180c5de034dd6c17555a0283929cf0c08feb0
Julia Wielińska <j.wielinska18@gmail.com>
2024-05-17 14:48:24 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,36 +24,42 @@ resource "azurerm_virtual_network" "vnetA" {
   location            = "West Europe"
   resource_group_name = "JulkaTest1"
   address_space       = ["10.0.0.0/23"]
-
-  subnet {
-    name           = "subnetA"
-    address_prefix = "10.0.0.0/24"
-  }
 }
+
+resource "azurerm_subnet" "subA" {
+    name           = "subnetA"
+    resource_group_name  = azurerm_resource_group.rg.name
+    virtual_network_name = azurerm_virtual_network.vnetA.name
+    address_prefixes = ["10.0.0.0/24"]
+  }
 
 resource "azurerm_virtual_network" "vnetB" {
   name                = "vnetB"
   location            = "West Europe"
   resource_group_name = "JulkaTest1"
   address_space       = ["10.1.0.0/23"]
-
-  subnet {
-    name           = "subnetB"
-    address_prefix = "10.1.1.0/24"
-  }
 }
+
+resource "azurerm_subnet" "subB" {
+    name           = "subnetB"
+    resource_group_name  = azurerm_resource_group.rg.name
+    virtual_network_name = azurerm_virtual_network.vnetB.name
+    address_prefixes = ["10.1.1.0/24"]
+  }
 
 resource "azurerm_virtual_network" "vnetC" {
   name                = "vnetC"
   location            = "West Europe"
   resource_group_name = "JulkaTest1"
   address_space       = ["10.2.0.0/23"]
-
-  subnet {
-    name           = "subnetC"
-    address_prefix = "10.2.0.0/24"
-  }
 }
+resource "azurerm_subnet" "subC" {
+    name           = "subnetC"
+    resource_group_name  = azurerm_resource_group.rg.name
+    virtual_network_name = azurerm_virtual_network.vnetC.name
+    address_prefixes = ["10.2.0.0/24"]
+  }
+
 
 resource "azurerm_virtual_network_peering" "VnetA-VnetB" {
   name                      = "VnetAVnetB"
@@ -97,6 +103,11 @@ resource "azurerm_route_table" "routetableA" {
   }
 }
 
+resource "azurerm_subnet_route_table_association" "sub1rt" {
+  subnet_id      = azurerm_subnet.subA.id
+  route_table_id = azurerm_route_table.routetableA.id
+}
+
 resource "azurerm_route_table" "routetableC" {
   name                          = "RouteTableC"
   location                      = azurerm_resource_group.rg.location
@@ -109,4 +120,9 @@ resource "azurerm_route_table" "routetableC" {
     next_hop_type  = "VirtualAppliance"
     next_hop_in_ip_address = "10.1.0.4"
   }
+}
+
+resource "azurerm_subnet_route_table_association" "sub2rt" {
+  subnet_id      = azurerm_subnet.subC.id
+  route_table_id = azurerm_route_table.routetableA.id
 }
